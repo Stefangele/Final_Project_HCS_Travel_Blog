c4a7829dd9e28baa8b4b910dc20db9763093c977
Stefan Angele <s.angele@more-marketing.de>
2022-12-05 17:45:14 +0100

    Update TravelCardDetailPage.js

--- travel_blog/src/TravelCardDetailPage.js
+++ travel_blog/src/TravelCardDetailPage.js
@@ -8,6 +8,9 @@ function TravelCardDetailPage() {
           src="/pictures_travellocations/image13.JPG"
         />
         <div class="hero-body">
+          <button class="button is-danger is-focused is-align-self-flex-start">
+            Go Back
+          </button>
           <div class="container">
             <h1 class="title is-size-1">Titel of Blog Post goes here</h1>
             <h2 class="subtitle">visiting date goes here</h2>
@@ -18,35 +21,51 @@ function TravelCardDetailPage() {
         <div class="container">
           <h2 class="title">Second Headline</h2>
           <h3 class="subtitle">
-            Here is a lot of lore ipsum text to be
-            written.........................................................................................................................................
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in
+            vulputate velit esse molestie consequat, vel illum dolore eu feugiat
+            nulla facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi. Lorem ipsum dolor sit amet,
           </h3>
         </div>
       </section>
 
-      <section class="level is-mobile">
+      <section class="level is-mobile pb-6">
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">City</p>
-            <p class="title is-size-4">Hamburg</p>
+            <p class="title is-size-5">Hamburg</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">Country</p>
-            <p class="title is-size-4">Germany</p>
+            <p class="title is-size-5">Germany</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">Highlight</p>
-            <p class="title is-size-4">Alster</p>
+            <p class="title is-size-5">Alster</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">Author</p>
-            <p class="title is-size-4">Stefan Angele</p>
+            <p class="title is-size-5">Stefan Angele</p>
           </div>
         </div>
       </section>
