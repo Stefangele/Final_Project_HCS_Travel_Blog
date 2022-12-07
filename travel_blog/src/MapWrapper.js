9c3c51af64fa3106defc3a3a5da7d5cd238ae605
Stefan Angele <s.angele@more-marketing.de>
2022-12-07 15:41:41 +0100

    Added Map Style

--- travel_blog/src/MapWrapper.js
+++ travel_blog/src/MapWrapper.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./Map.css";
+// import geoJson from "./chicago-parks.json";
 
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
@@ -10,28 +11,32 @@ mapboxgl.accessToken =
 function MapWrapper() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(9.81);
+  const [lat, setLat] = useState(53.1);
+  const [zoom, setZoom] = useState(6.5);
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return;
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: "mapbox://styles/mapbox/outdoors-v12",
       center: [lng, lat],
       zoom: zoom,
     });
   });
 
   useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
+    if (!map.current) return;
     map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
   });
+
+  // geoJson.features.map((feature) =>
+  //   new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(map)
+  // );
 
   return (
     <div className="column">
@@ -40,6 +45,9 @@ function MapWrapper() {
           Places I've been to
         </h2>
         <div>
+          <div className="sidebar">
+            Längengrad: {lng} | Breitengrad: {lat} | Zoomlevel: {zoom}
+          </div>
           <div ref={mapContainer} className="map-container" />
         </div>
       </div>
